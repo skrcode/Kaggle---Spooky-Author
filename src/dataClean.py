025b0d552e0f09a481e99ea4e4e81506f9a15cda
Suraj Krishnan Rajan <surajkrajan95@gmail.com>
2017-10-27 22:52:20 +0530

    Got sentence vectors by averaging

--- src/dataClean.py
+++ src/dataClean.py
@@ -1,7 +1,23 @@
 from nltk.corpus import stopwords
 import re
 import pandas as pd
+import nltk.data
+import nltk
+import numpy as np
 
-def do(X_train,X_test):
+def getWordVectors(X_train_features,X_test_features,word_vecs):
+	X_train_features['word_vectors'] = [ [ word_vecs[word] for word in sentence if word in word_vecs] for sentence in X_train_features['text']]
+	X_test_features['word_vectors'] = [ [ word_vecs[word] for word in sentence if word in word_vecs] for sentence in X_test_features['text']] 
+	return X_train_features,X_test_features
 
-	return X_train,X_test
+def getSentenceVectors(X_train_features,X_test_features):
+	X_train_features['sentence_vectors'] =[np.mean(sentence,axis = 0) for sentence in X_train_features['word_vectors']]
+	X_test_features['sentence_vectors'] =[np.mean(sentence,axis = 0) for sentence in X_test_features['word_vectors']] 
+	return X_train_features,X_test_features
+
+def clean(X_train,X_test):
+	X_train_features = X_train.copy()
+	X_test_features = X_test.copy()
+	X_train_features['text'] = [re.sub("[^a-zA-Z]"," ", data).lower().split() for data in X_train['text']]
+	X_test_features['text'] = [re.sub("[^a-zA-Z]"," ", data).lower().split() for data in X_test['text']]
+	return X_train_features,X_test_features  
--- src/dataRead.py
+++ src/dataRead.py
@@ -2,11 +2,18 @@ import pandas as pd
 import sys
 import glob
 import errno
+import csv
+import numpy as np
 
 def do():
 	train = "../data/train.csv"
 	test = "../data/test.csv"
+	wv = "../../../../glove.6B/glove.6B.50d.txt"
 	X_train = pd.read_csv( train, header=0,delimiter="," )
 	X_test = pd.read_csv( test, header=0,delimiter="," )
-
-	return X_train,X_test
+	word_vecs = {}
+	with open(wv) as f:
+	    for line in f:
+	       vals = line.split()
+	       word_vecs[vals[0]] = np.array(vals[1::],dtype=float)
+	return X_train,X_test,word_vecs
