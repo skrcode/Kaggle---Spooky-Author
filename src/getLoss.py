65b175b1f20c701779a8deeec0bdadaa54f84a67
Suraj Krishnan Rajan <surajkrajan95@gmail.com>
2017-11-17 00:12:45 +0530

    reorganized

--- src/getLoss.py
+++
@@ -1,8 +0,0 @@
-from sklearn.metrics import log_loss
-import numpy as np
-import pandas as pd
-def do(result,X_train_features):
-	eap = log_loss(np.array(pd.get_dummies(X_train_features['author'])['EAP']), np.array(result['EAP']))
-	mws = log_loss(np.array(pd.get_dummies(X_train_features['author'])['MWS']), np.array(result['MWS']))
-	hpl = log_loss(np.array(pd.get_dummies(X_train_features['author'])['HPL']), np.array(result['HPL']))
-	return 'EAP:',eap,'MWS:',mws,'HPL:',hpl 
